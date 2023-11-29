ee02fc848e2fae3df532b4a7d0395314bae43080
MShaw1223 <134694154+MShaw1223@users.noreply.github.com>
2023-11-29 09:55:49 +0000

    small change

--- server.js
+++ server.js
@@ -4,8 +4,8 @@ const app = express();
 
 app.use("/static", express.static(path.resolve(__dirname, "frontend", "static"))); //surfs dir. if url contains static
 
-app.get("/*", (req,res)=>{
+app.get("/*", (req, res) => {
     res.sendFile(path.resolve(__dirname, "frontend", "index.html"));
 });
 
-app.listen(process.env.PORT || 1080, ()=>console.log("Server running"));
+app.listen(process.env.PORT || 1080, () =>console.log("Server running"));
